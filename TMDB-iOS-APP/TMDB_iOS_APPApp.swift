a81b68c7f11dcfb1f055e18306a59d6d7f4c79ad
fatihkhrmn <ffatihh.11@hotmail.com>
2024-09-04 12:42:50 +0300

    Initial Commit

---
+++ TMDB-iOS-APP/TMDB_iOS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_iOS_APPApp.swift
+//  TMDB-iOS-APP
+//
+//  Created by Fatih Kahraman on 4.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_iOS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
