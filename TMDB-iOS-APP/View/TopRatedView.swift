abdee4eed07a7b8a3d2be5cff0cb941b4b463f77
fatihkhrmn <ffatihh.11@hotmail.com>
2024-09-11 11:34:48 +0300

    files added

---
+++ TMDB-iOS-APP/View/TopRatedView.swift
@@ -0,0 +1,18 @@
+//
+//  TopRatedView.swift
+//  TMDB-iOS-APP
+//
+//  Created by Fatih Kahraman on 11.09.2024.
+//
+
+import SwiftUI
+
+struct TopRatedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopRatedView()
+}
---
+++ TMDB-iOS-APP/ViewModel/UpcomingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UpcomingViewModel.swift
+//  TMDB-iOS-APP
+//
+//  Created by Fatih Kahraman on 11.09.2024.
+//
+
+import Foundation
